926c91290ac0486c5db8fe6995afdd0278f28b1f
nikmet <metlov.nm@yandex.ru>
2024-04-12 20:59:41 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+class App {
+    routes = [{ path: "", view: null }];
+
+    constructor() {
+        window.addEventListener('hashchange', this.route.bind(this))
+        this.route()
+    }
+
+    route() {
+        
+    }
+}
+
+new App();
